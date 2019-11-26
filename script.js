e08f38dbf2cb2cca064f28071b340f0eafd844ba
Abhishek Nagekar <abhishek@nagekar.com>
2019-11-26 11:13:16 +0100

    Added input handlers

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert('hello world')
+let submitButton = document.querySelector('#submit-button');
+function clickListener(event) {
+	event.preventDefault();
+	let emailInput = document.querySelector('#email');
+	let messageInput = document.querySelector('#message');
+
+	let emailText = emailInput.value;
+	let messageText = messageInput.value;
+
+	console.log('email:', emailText, 'message:', messageText);
+}
+submitButton.addEventListener('click', clickListener);
